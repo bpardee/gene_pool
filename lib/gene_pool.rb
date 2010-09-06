58f5b164a330a83627f3dcf844a5b3683854a3e2
Brad Pardee <bradpardee@gmail.com>
2010-09-05 20:20:47 -0400

    Version bump to 0.0.0

---
+++ lib/gene_pool.rb
@@ -0,0 +1,134 @@
+# Generic connection pool class
+class GenePool
+
+  attr_reader :name, :pool_size, :warn_timeout, :logger
+
+  def initialize(options={}, &connect_block)
+    @connect_block = connect_block
+
+    @name         = options[:name]         || 'GenePool'
+    @pool_size    = options[:pool_size]    || 1
+    @warn_timeout = options[:warn_timeout] || 5.0
+    @logger       = options[:logger]
+
+    # Mutex for synchronizing pool access
+    @mutex = Mutex.new
+
+    # Condition variable for waiting for an available connection
+    @queue = ConditionVariable.new
+
+    @connections = []
+    @checked_out = []
+    # Map the original connections object_id within the with_connection method to the final connection.
+    # This could change if the connection is renew'ed.
+    @with_map    = {}
+  end
+
+  # Check out a connection from the pool, creating it if necessary.
+  def checkout
+    start_time = Time.now
+    connection = nil
+    reserved_connection_placeholder = Thread.current
+    begin
+      @mutex.synchronize do
+        until connection do
+          if @checked_out.size < @connections.size
+            connection = (@connections - @checked_out).first
+            @checked_out << connection
+          elsif @connections.size < @pool_size
+            # Perform the actual connection outside the mutex
+            connection = reserved_connection_placeholder
+            @connections << connection
+            @checked_out << connection
+            @logger.debug "#{@name}: Created connection ##{@connections.size} #{connection}:#{connection.object_id} for #{name}" if @logger
+          else
+            @logger.debug "#{@name}: Waiting for an available connection, all #{@pool_size} connections are checked out." if @logger
+            @queue.wait(@mutex)
+          end
+        end
+      end
+    ensure
+      delta = Time.now - start_time
+      if @logger && delta > @warn_timeout
+        @logger.warn "#{@name}: It took #{delta} seconds to obtain a connection.  Consider raising the pool size which is " +
+          "currently set to #{@pool_size}."
+      end
+    end
+    if connection == reserved_connection_placeholder
+      connection = renew(reserved_connection_placeholder)
+    end
+    
+    @logger.debug "#{@name}: Checkout connection #{connection.object_id} self=#{self}" if @logger
+    return connection
+  end
+
+  # Return a connection to the pool.
+  def checkin(connection)
+    @mutex.synchronize do
+      @checked_out.delete(connection)
+      @queue.signal
+    end
+    @logger.debug "#{@name}: Checkin connection #{connection.object_id} self=#{self}" if @logger
+  end
+  
+  # Create a scope for checking out a connection
+  def with_connection
+    connection = checkout
+    @mutex.synchronize do
+      @with_map[connection.object_id] = connection
+    end
+    begin
+      yield connection
+    ensure
+      @mutex.synchronize do
+        # Update connection for any renew's that have occurred
+        connection = @with_map.delete(connection.object_id)
+      end
+      checkin(connection)
+    end
+  end
+  
+  # Remove an existing connection from the pool
+  def remove(connection)
+    @mutex.synchronize do
+      @connections.delete(connection)
+      @checked_out.delete(connection)
+      @queue.signal
+    end
+    @logger.debug "#{@name}: Removed connection #{connection.object_id} self=#{self}" if @logger
+  end
+
+  # If a connection needs to be renewed for some reason, reassign it here
+  def renew(old_connection)
+    new_connection =
+      begin
+        @connect_block.call
+      rescue Exception
+        remove old_connection
+        raise
+      end
+    @mutex.synchronize do
+      index = @checked_out.index(old_connection)
+      raise Error.new("Can't reassign non-checked out connection for #{@name}") unless index
+      @checked_out[index] = new_connection
+      @connections[@connections.index(old_connection)] = new_connection
+      # If this is part of a with_connection block, then track our new connection
+      with_key = @with_map.index(old_connection)
+      @with_map[with_key] = new_connection if with_key
+    end
+    @logger.debug "#{@name}: Renewed connection old=#{old_connection.object_id} new=#{new_connection}:#{new_connection.object_id}" if @logger
+    return new_connection
+  end
+  
+  # Perform the given block for each connection, i.e., closing each connection.
+  def each
+    @connections.each { |connection| yield connection }
+  end
+  
+  def to_s
+    conn = @connections.map{|c| c.object_id}.join(',')
+    chk  = @checked_out.map{|c| c.object_id}.join(',')
+    with = @with_map.keys.map{|k| "#{k}=#{@with_map[k].object_id}"}.join(',')
+    "connections=#{conn} checked_out=#{chk} with_map=#{with}"
+  end
+end
---
+++ test/gene_pool_test.rb
@@ -0,0 +1,232 @@
+require 'rubygems'
+require 'test/unit'
+require 'shoulda'
+require 'gene_pool'
+require 'stringio'
+require 'logger'
+require 'timeout'
+
+# Increase visibility
+class GenePool
+  attr_reader :connections, :checked_out, :with_map
+end
+
+class DummyConnection
+  def initialize(count, sleep_time=nil)
+    sleep sleep_time if sleep_time
+    @count = count
+  end
+  
+  def to_i
+    @count
+  end
+  
+  def to_s
+    @count.to_s
+  end
+end
+    
+
+class GenePoolTest < Test::Unit::TestCase
+
+  context 'on default setup' do
+    setup do
+      @gene_pool = GenePool.new { Object.new }
+    end
+
+    should 'have default options set' do
+      assert_equal 'GenePool', @gene_pool.name
+      assert_equal 1,          @gene_pool.pool_size
+      assert_equal 5.0,        @gene_pool.warn_timeout
+      assert_nil               @gene_pool.logger
+    end
+  end
+  
+  context '' do
+    setup do
+      @stringio = StringIO.new
+      #@logger = Logger.new($stdout)
+      #@logger = Logger.new(@stringio)
+      @logger = nil
+      # Override sleep in individual tests
+      @sleep = nil
+      @timeout = 5
+      counter = 0
+      mutex = Mutex.new
+      @gene_pool = GenePool.new(:name         => 'TestGenePool',
+                                :pool_size    => 10,
+                                :warn_timeout => 2.0,
+                                :logger       => @logger) do
+        count = nil
+        mutex.synchronize do
+          count = counter += 1
+        end
+        Timeout.timeout(@timeout) do
+          DummyConnection.new(count, @sleep)
+        end
+      end
+    end
+
+    should 'have options set' do
+      assert_equal 'TestGenePool', @gene_pool.name
+      assert_equal 10,            @gene_pool.pool_size
+      assert_equal 2.0,            @gene_pool.warn_timeout
+      assert_same  @logger,        @gene_pool.logger
+    end
+
+    should 'create 1 connection' do
+      (1..3).each do |i|
+        @gene_pool.with_connection do |conn|
+          assert_equal conn.to_i, 1
+          assert_equal 1,         @gene_pool.connections.size
+          assert_equal 1,         @gene_pool.checked_out.size
+          assert_same  conn,      @gene_pool.connections[0]
+          assert_same  conn,      @gene_pool.checked_out[0]
+        end
+        assert_equal 0, @gene_pool.checked_out.size
+      end
+    end
+  
+    should 'create 2 connections' do
+      conn1 = @gene_pool.checkout
+      (1..3).each do |i|
+        @gene_pool.with_connection do |conn2|
+          assert_equal 1, conn1.to_i
+          assert_equal 2, conn2.to_i
+          assert_equal 2, @gene_pool.connections.size
+          assert_equal 2, @gene_pool.checked_out.size
+          assert_same  conn1, @gene_pool.connections[0]
+          assert_same  conn1, @gene_pool.checked_out[0]
+          assert_same  conn2, @gene_pool.connections[1]
+          assert_same  conn2, @gene_pool.checked_out[1]
+        end
+        assert_equal 1, @gene_pool.checked_out.size
+      end
+      @gene_pool.checkin(conn1)
+      assert_equal 0, @gene_pool.checked_out.size
+    end
+  
+    should 'be able to reset multiple times' do
+      @gene_pool.with_connection do |conn1|
+        conn2 = @gene_pool.renew(conn1)
+        conn3 = @gene_pool.renew(conn2)
+        assert_equal 1, conn1.to_i
+        assert_equal 2, conn2.to_i
+        assert_equal 3, conn3.to_i
+        assert_equal 1, @gene_pool.connections.size
+        assert_equal 1, @gene_pool.checked_out.size
+      end
+      assert_equal 1, @gene_pool.connections.size
+      assert_equal 0, @gene_pool.checked_out.size
+    end
+  
+    should 'be able to remove connection' do
+      @gene_pool.with_connection do |conn|
+        @gene_pool.remove(conn)
+        assert_equal 0, @gene_pool.connections.size
+        assert_equal 0, @gene_pool.checked_out.size
+      end
+      assert_equal 0, @gene_pool.connections.size
+      assert_equal 0, @gene_pool.checked_out.size
+    end
+  
+    should 'be able to remove multiple connections' do
+      @gene_pool.with_connection do |conn1|
+        @gene_pool.with_connection do |conn2|
+          @gene_pool.with_connection do |conn3|
+            @gene_pool.remove(conn1)
+            @gene_pool.remove(conn3)
+            assert_equal 1, @gene_pool.connections.size
+            assert_equal 1, @gene_pool.checked_out.size
+            assert_same  conn2, @gene_pool.checked_out[0]
+            assert_same  conn2, @gene_pool.connections[0]
+          end
+          assert_equal 1, @gene_pool.connections.size
+          assert_equal 1, @gene_pool.checked_out.size
+        end
+        assert_equal 1, @gene_pool.connections.size
+        assert_equal 0, @gene_pool.checked_out.size
+      end
+      assert_equal 1, @gene_pool.connections.size
+      assert_equal 0, @gene_pool.checked_out.size
+    end
+  
+    should 'handle aborted connection' do
+      @gene_pool.with_connection do |conn1|
+        @sleep = 2
+        @timeout = 1
+        begin
+          @gene_pool.with_connection { |conn2| }
+          flunk "connection should have timed out"
+        rescue  Timeout::Error => e
+          #pass
+        end
+        assert_equal 1, @gene_pool.connections.size
+        assert_equal 1, @gene_pool.checked_out.size
+      end
+      assert_equal 1, @gene_pool.connections.size
+      assert_equal 0, @gene_pool.checked_out.size
+      # Do another test just to be sure nothings hosed
+      @sleep = nil
+      @gene_pool.with_connection do |conn1|
+        assert 1, conn1.to_i
+      end
+    end
+  
+    should 'not allow more than pool_size connections' do
+      conns = []
+      pool_size = @gene_pool.pool_size
+      (1..pool_size).each do |i|
+        c = @gene_pool.checkout
+        conns << c
+        assert_equal i, c.to_i
+        assert_equal i, @gene_pool.connections.size
+        assert_equal i, @gene_pool.checked_out.size
+        assert_equal conns, @gene_pool.connections
+      end
+      begin
+        Timeout.timeout(1) do
+          @gene_pool.checkout
+        end
+        flunk "connection should have timed out"
+      rescue  Timeout::Error => e
+        #pass "successfully timed out connection"
+      end
+      (1..pool_size).each do |i|
+        @gene_pool.checkin(conns[i-1])
+        assert_equal pool_size,   @gene_pool.connections.size
+        assert_equal pool_size-i, @gene_pool.checked_out.size
+      end
+    end
+
+    should 'handle thread contention' do
+      conns = []
+      pool_size = @gene_pool.pool_size
+      # Do it with new connections and old connections
+      (1..2).each do |n|
+        (1..pool_size).each do |i|
+          Thread.new do
+            c = @gene_pool.checkout
+            conns[i-1] = c
+          end
+        end
+        # Let the threads complete
+        sleep 1
+        assert_equal pool_size,   @gene_pool.connections.size
+        assert_equal pool_size,   @gene_pool.checked_out.size
+        (1..pool_size).each do |i|
+          Thread.new do
+            @gene_pool.checkin(conns[i-1])
+          end
+        end
+        sleep 1
+        assert_equal pool_size, @gene_pool.connections.size
+        assert_equal 0,         @gene_pool.checked_out.size
+      end
+      ival_conns = []
+      @gene_pool.each { |conn| ival_conns << conn.to_i }
+      ival_conns.sort!
+      assert_equal (1..pool_size).to_a, ival_conns
+    end
+  end
+end
